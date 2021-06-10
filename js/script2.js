6f19a347b2d749667ae44966023a9978b3687564
gianmarco <gianmarco.palombo600@gmail.com>
2021-06-10 17:31:19 +0200

    quasi arrivvati

---
+++ js/script2.js
@@ -0,0 +1,91 @@
+// var semaforoDiff = 0;
+var max;
+var mine;
+var attemptArr = [];
+start();
+var printOutput = '';
+printOutput = document.getElementById('output').innerHTML;
+
+function start(){
+    document.getElementById("inizia").addEventListener('click' , diff);
+}
+// console.log(max);
+
+
+
+function diff(){
+    
+       
+    if(document.getElementById("facile").checked == true){
+        max = 100;
+    }
+    else if(document.getElementById("medio").checked == true){
+        max = 80;
+    }
+    else if(document.getElementById("difficile").checked == true){
+        max = 50;
+    }
+    document.getElementById("diff").classList.add("_hidden");  
+    document.getElementById("input").classList.remove("_hidden");  
+    console.log(max);
+    
+
+    document.getElementById('label').innerHTML = 'Inserisci un numero da 1 a ' + max;
+    
+    mine = getRandomArr(max);
+    console.log(mine);
+    document.getElementById('inviatentativo').addEventListener('click' , getValue );
+}
+
+function getRandomArr(massimo){
+    var randomArr = [];
+    for(var i = 0; i < 16; i++){
+        var numR = Math.floor(Math.random()* massimo) 
+        randomArr.push(numR);
+    }
+    return randomArr;
+}
+
+function getValue(){
+    document.getElementById("console").classList.remove("_hidden")
+    var attempt = parseInt(document.getElementById('attempt').value);
+    if(attempt < 0 || attempt > max){
+        document.getElementById('output').innerHTML += "<li class='text-center'>Puoi inserire solo numeri fino a " + max + "</li>";
+    }
+    else{
+        if(mine.includes(attempt)){
+            alert();
+            document.getElementById('inviatentativo').removeEventListener('click' , getValue , false );
+            
+            document.getElementById("input").classList.add("_hidden");  
+            
+            document.getElementById("console").classList.add("_hidden")
+            document.getElementById("persa").classList.remove("_hidden"); 
+            document.getElementById('fine').innerHTML = "OPS! Sei esploso al tentativo numero " + attemptArr.length+1;
+            document.getElementById('output').innerHTML = "";
+            document.getElementById('riprova').addEventListener('click' , beforeStart);
+
+        }
+        else if(attemptArr.includes(attempt)){
+            document.getElementById('output').innerHTML += "<li class='text-center'>Non credo tu abbia davvero inserito un numero, ritenta</li>";
+            
+        }
+        else if (isNaN(attempt)){
+            document.getElementById('output').innerHTML += "<li class='text-center'>Non credo tu abbia davvero inserito un numero, ritenta</li>";
+        }
+        else{
+            attemptArr.push(attempt);
+            document.getElementById('output').innerHTML += "<li class='text-center'>Hai inserito il numero " + attempt + ". <strong>Tentativo numero " + attemptArr.length + "</strong></li>";
+        }
+        }
+    document.getElementById('attempt').value = "";
+    console.log(attemptArr);
+    
+}
+
+function beforeStart(){
+    document.getElementById("diff").classList.remove("_hidden");  
+    attemptArr = [];
+    document.getElementById("persa").classList.add("_hidden"); 
+    start()
+}
